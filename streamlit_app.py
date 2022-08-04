974d3ec6e75589dc86e7fd94cba59ad4b9f05fd9
acirtep-snowflake <110589906+acirtep-snowflake@users.noreply.github.com>
2022-08-04 13:50:54 +0200

    add button to query snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,15 +40,17 @@ try:
 except URLError as e:
   streamlit.error()
 
+  
+def get_fruit_list(my_cnx):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+    
 
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
+if streamlit.button("Get fruit list"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_list(my_cnx)
+  streamlit.dataframe(my_data_rows)
 
 fruit_add = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', fruit_add)
