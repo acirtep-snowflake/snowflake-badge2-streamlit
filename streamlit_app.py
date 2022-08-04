7259c76c50e70bc4083ee94e02979efc368f9f88
acirtep-snowflake <110589906+acirtep-snowflake@users.noreply.github.com>
2022-08-04 08:44:43 +0200

    Add menu information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title("Snowflake Badge 2 workshop")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
